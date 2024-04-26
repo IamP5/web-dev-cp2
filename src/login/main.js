2cf865193f975a36685ef517809a9cf4c6227e2b
Tuba <brunogabrielsd@hotmail.com>
2024-04-26 20:53:18 -0300

    feat: login
    redireciona para página home caso o login seja correto

---
+++ src/login/main.js
@@ -0,0 +1,21 @@
+const FORM = document.getElementById('login-form');
+
+FORM.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const data = new FormData(FORM);
+  const username = data.get('username');
+  const password = data.get('password');
+
+  if (username === '1234' && password === '1234') {
+    clearInputs();
+    window.location.assign("http://" + window.location.host + '/src' + "/home");
+  } else {
+    clearInputs();
+    document.getElementById('error-message').innerText = 'Invalid username or password.';
+  }
+});
+
+function clearInputs() {
+  FORM.reset();
+}
